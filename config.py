ce7b175d8478428d2e25ba43186aebfa89a4dc4b
robin <robinwang08@gmail.com>
2019-10-01 14:50:34 -0700

    edit config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     IMAGE_SIZE = 200
 
     # Should change trials to 10
-    TRIALS = 10
+    TRIALS = 20 
     BATCH_SIZE = 16
 
     EPOCHS = 500
@@ -15,33 +15,37 @@ class Config(object):
     VALIDATION_SPLIT = 0.2
     TEST_SPLIT = 0.1
 
+    EXPERTS = "/media/user1/my4TB/robin/ovarian/ovarian_data/csv/Ovarian_Experts.csv"
+
     DEVELOPMENT = True
     DEBUG = True
     PRINT_SQL = False
     SECRET = "example secret key"
     LOG_LEVEL = logging.DEBUG
 
-    RAW_NRRD_ROOT = "/home/user1/robin/ovarian/ovarian_data/raw"
+    RAW_NRRD_ROOT = "/media/user1/my4TB/robin/ovarian/ovarian_data/raw"
     RAW_FEATURES = [
-        "/home/user1/robin/ovarian/ovarian_data/csv/Ovarian_Outcome.csv",
-        "/home/user1/robin/ovarian/ovarian_data/csv/Ovarian_Institution.csv",
+        "/media/user1/my4TB/robin/ovarian/ovarian_data/csv/Ovarian_Outcome.csv",
+        "/media/user1/my4TB/robin/ovarian/ovarian_data/csv/Ovarian_Institution.csv",
+        "/media/user1/my4TB/robin/ovarian/ovarian_data/csv/Ovarian_Clinical.csv",
+        "/media/user1/my4TB/robin/ovarian/ovarian_data/csv/Ovarian_Sort.csv",
         # "C:/research/ovarian/ovarian_data/csv/",
         ]
 
-    DATA = "/home/user1/robin/ovarian/ovarian_data/data"
+    DATA = "/media/user1/my4TB/robin/ovarian/ovarian_data/data"
     PREPROCESSED_DIR = os.path.join(DATA, "preprocessed")
     TRAIN_DIR = os.path.join(DATA, "train")
     TEST_DIR = os.path.join(DATA, "test")
     VALIDATION_DIR = os.path.join(DATA, "validation")
 
-    FEATURES_DIR = "/home/user1/robin/ovarian/ovarian_data/features"
+    FEATURES_DIR = "/media/user1/my4TB/robin/ovarian/ovarian_data/features"
     NRRD_FEATURES = os.path.join(FEATURES_DIR, "nrrd-features.pkl")
     FEATURES = os.path.join(FEATURES_DIR, "training-features.pkl")
     PREPROCESS = os.path.join(FEATURES_DIR, "preprocess.pkl")
 
     INPUT_FORM = "all"
 
-    OUTPUT = "/home/user1/robin/ovarian/ovarian_data/output"
+    OUTPUT = "/media/user1/my4TB/robin/ovarian/ovarian_data/output"
     DB_URL = "sqlite:///{}/results.db".format(OUTPUT)
     MODEL_DIR = os.path.join(OUTPUT, "models")
     STDOUT_DIR = os.path.join(OUTPUT, "stdout")
